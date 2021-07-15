fa0dbc6ad62197355e82d394010d7b2b8a38acc7
Srikrishnan V <shyam28598@gmail.com>
2021-07-15 18:04:32 +0530

    added graph and id

--- assignment.py
+++ assignment.py
@@ -1,13 +1,16 @@
-import os
-import glob
+import os,json
 
-path = './repos/iudx-voc/'
-contents = []
-for root, directories, files in os.walk(path):
+path_to_json = './repos/iudx-voc/'
+
+graphs = []
+id= []
+for root, directories, files in os.walk(path_to_json):
     for file in files:
         filepath = os.path.join(root, file)
         if filepath.endswith('.jsonld'):
-            with open(file,'r') as filecontent:
-                contents.append(file)
-                print(contents)
-            
+            with open(filepath) as json_file:
+                data = json.load(json_file)
+                if '@graph' in data:
+                    graphs.append(data['@graph'])
+                    id.append(data['@graph'][0]['@id'])
+                    print(id)
