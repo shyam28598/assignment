75dc8b83d57ca3e450a7d8b7d2c7ae117c1179d5
shyam28598 <shyam28598@gmail.com>
2021-07-28 11:55:24 +0530

    added list jsonld

--- graph.py
+++ graph.py
@@ -4,12 +4,12 @@ import os
 import json
 
 
-path_to_json='./repos/iudx-voc/'
-json_ld_graph=[]
-classes=['owl:Class', 'rdfs:Class']
-properties= ["iudx:TextProperty", "iudx:QuantitativeProperty", "iudx:StructuredProperty", "iudx:GeoProperty", "iudx:TimeProperty", "rdf:Property"] 
-relation=["iudx:Relationship"]
-
+path_to_json ='./repos/iudx-voc/'
+json_ld_graph = []
+classes = ['owl:Class', 'rdfs:Class']
+properties = ["iudx:TextProperty", "iudx:QuantitativeProperty", "iudx:StructuredProperty", "iudx:GeoProperty", "iudx:TimeProperty", "rdf:Property"] 
+relation = ["iudx:Relationship"]
+blank = []
 for subdir, dirs, files in os.walk(path_to_json):
     for file in files:
         #print os.path.join(subdir, file)
@@ -18,7 +18,7 @@ for subdir, dirs, files in os.walk(path_to_json):
         if filepath.endswith(".jsonld"):
             # print (filepath)
             with open(filepath,"r+") as input_file:
-                data=json.load(input_file)
+                data = json.load(input_file)
                 if "@graph" in data:
                     json_ld_graph.append((data["@graph"][0])) 
                     
@@ -28,15 +28,16 @@ for subdir, dirs, files in os.walk(path_to_json):
 
 
 class Vertex:
-    def __init__(self, node, vertice_type) -> None:
+    def __init__(self, node, vertice_type, jsonld) -> None:
         self.id = node
         self.node_type = vertice_type
-        self.adjacent= {}
-       
+        self.adjacent = {}
+        self.jsonlist = list(jsonld)
+
     def __str__(self) -> str:
         print(str(self.id))
 
-    def add_neighbour(self,neighbour,relationship):
+    def add_neighbour(self, neighbour, relationship):
         self.adjacent[neighbour] = relationship
     
     def get_connections(self):
@@ -62,22 +63,22 @@ class Graph:
     def __iter__(self) ->None:
         return(iter(self.vertices.values()))
 
-    def add_vertex(self, node, tp):
+    def add_vertex(self, node, tp, jsonld):
         self.num_of_vertices = self.num_of_vertices + 1
-        new_vertex = Vertex(node, tp)
-        self.vertices[node] =new_vertex
+        new_vertex = Vertex(node, tp, jsonld)
+        self.vertices[node] = new_vertex
         return new_vertex
 
     def add_edge(self,vertex_from, vertex_to, relationship): 
         self.vertices[vertex_from].add_neighbour(self.vertices[vertex_to], relationship)
         
-    def get_subclasses(self,v):
+    def get_class_graph(self, v, out = []):
         for key, value in v.adjacent.items():
-            if value=="domainOf":
-                print(key.id)
-            elif value=="subClassOf":
-                print(key.id)
-                self.get_subclasses(key)
+            if value == "domainOf":
+                out.append(key.id)
+            elif value == "subClassOf":
+                out.append(key.id)
+                self.get_class_graph(key)
 
     def get_vertex(self, search):
         if search in self.vertices:
@@ -96,12 +97,12 @@ for n in json_ld_graph:
     try:
         # Making vertices of all classes  
         if (any(ele in classes for ele in n["@type"])):
-            tp="Class"
-            g.add_vertex(n["@id"], tp)
+            tp = "Class"
+            g.add_vertex(n["@id"], tp, n)
         # Making vertices of all properties
         if (any(ele in properties for ele in n["@type"])):
             tp = "Property"
-            g.add_vertex(n["@id"], tp)
+            g.add_vertex(n["@id"], tp, n)
     except:
         pass     
 
@@ -120,6 +121,8 @@ for n in json_ld_graph:
                 except:
                     pass
                 
-  
+
+
 n = g.get_vertex("iudx:Resource")
-g.get_subclasses(n)
+g.get_class_graph(n,blank)
+print(blank)
